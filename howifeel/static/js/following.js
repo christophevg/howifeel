7a96edd66807028b5b8ccc082d58a1a9ba850b1a
Christophe VG <contact@christophe.vg>
2022-02-07 21:00:09 +0100

    added fourth mood + centralised configuration

--- howifeel/static/js/following.js
+++ howifeel/static/js/following.js
@@ -10,36 +10,24 @@ function unfollow(username) {
   });
 }
 
-function show_followed(followed) {
-  var color = {
-    "super" : "success",
-    "ok"    : "warning",
-    "down"  : "danger"
-  }[followed.mood];
-  var text = {
-    "super" : "I'm Super!<br>I'm feeling just fine.",
-    "ok"    : "I'm Ok.<br>Everything is calm, normal.",
-    "down"  : "I'm Down.<br>I'm not feeling too good."
-  }[followed.mood];
-  $("#following").append(`
-<div  id="followed-${followed.user}" class="card mb-4 rounded-3 shadow-sm border-${color} text-center">
-  <div class="card-body d-flex justify-content-between align-items-center bg-${color} border-${color} text-white">
-    <span>
-      <img src="https://www.gravatar.com/avatar/${followed.gravatar}?d=mp" alt="mdo" width="32" height="32" class="bg-light rounded-circle">
-      <br>
-      ${followed.user}
-    </span>
-    <span>${text}
-    </span>
-    <button type="button" onclick="unfollow('${followed.user}');" class="pull-right text-white btn btn-lg">&times;</button>
-  </div>
-</div>
-`);  
-}
-
-// get our following
-$.get("/api/me/following", function(following) {
-  $.each(following, function(index, followed) {
-    show_followed(followed);
+function show_followed() {
+  $.get("/api/me/following", function(following) {
+    $.each(following, function(index, followed) {
+      var mood  = moods[followed.mood],
+          color = mood.color;
+      $("#following").append(`
+    <div  id="followed-${followed.user}" class="card mb-4 rounded-3 shadow-sm border-${color} text-center">
+      <div class="card-body d-flex justify-content-between align-items-center bg-${color} border-${color} text-white">
+        <span>
+          <img src="https://www.gravatar.com/avatar/${followed.gravatar}?d=mp" alt="mdo" width="32" height="32" class="bg-light rounded-circle">
+          <br>
+          ${followed.user}
+        </span>
+        <span>${mood.header}<br>${mood.body}</span>
+        <button type="button" onclick="unfollow('${followed.user}');" class="pull-right text-white btn btn-lg">&times;</button>
+      </div>
+    </div>
+      `);
+    });
   });
-});
+}
--- howifeel/static/js/mood.js
+++ howifeel/static/js/mood.js
@@ -1,46 +1,95 @@
-// mapping of our moods to bootstrap colors
-var mood_colors = {
-  "super" : "success",
-  "ok"    : "warning",
-  "down"  : "danger"
+// mapping of our moods to bootstrap colors and emoji's
+var moods = {
+  "super" : {
+    "color"  : "success",
+    "header" : "&#128525;",
+    "body"   : "I'm Super!<br>I'm feeling just fine.",
+    "action" : "Super!"
+  },
+  "ok"    : {
+    "color"  : "warning",
+    "header" : "&#128512;",
+    "body"   : "I'm Ok.<br>Everything is calm, normal.",
+    "action" : "Ok"
+  },
+  "nok"   : {
+    "color"  : "warning",
+    "header" : "&#128528;",
+    "body"   : "I'm Ok on the surface.<br>I hope things clear up a bit.",
+    "action" : "Ok"
+  },
+  "down"  : {
+    "color"  : "danger",
+    "header" : "&#128530;",
+    "body"   : "I'm Down.<br>I'm not feeling too good.",
+    "action" : "Down"
+  }
 };
 
-function activate_mood(mood) {
-  var panel = $("#" + mood);
-  panel.find(".card-header").addClass("bg-" + mood_colors[mood]);
-
-  var button = panel.find("button");
-  button.prop("disabled", true);
-  button.removeClass("btn-outline-" + mood_colors[mood]);
-  button.addClass("btn-" + mood_colors[mood]);
-}
-
-function deactivate_mood(mood) {
-  var panel = $("#" + mood);
-  panel.find(".card-header").removeClass("bg-" + mood_colors[mood]);
-
-  var button = panel.find("button");
-  button.prop("disabled", false);
-  button.addClass("btn-outline-" + mood_colors[mood]);
-  button.removeClass("btn-" + mood_colors[mood]);
-}
-
-function set_mood(mood) {
-  post("/api/mood", { "mood" : mood }, function() {
-    deactivate_mood("super");
-    deactivate_mood("ok");
-    deactivate_mood("down");
-
+function show_mood_selectors() {
+  // get current mood to intialize page
+  $.get("/api/mood", function(mood) {
+    var count = moods.length;
+    $("#moods").removeClass("row-cols-md-2 mb-2 row-cols-md-3 mb-3 row-cols-md-4 mb-4");
+    $("#moods").addClass(`row-cols-md-${count} mb-${count}`)
+    $.each(moods, function(id, mood) {
+      $("#moods").append(`
+        <div id="${id}" class="col">
+          <div class="card mb-4 rounded-3 shadow-sm border-${mood.color}">
+            <div class="card-header py-3 border-${mood.color}">
+              <h4 class="my-0 fw-normal">${mood.header}</h4>
+              <button type="button" class="w-100 btn btn-lg btn-outline-${mood.color}">${mood.action}</button>
+            </div>
+          </div>
+        </div>
+      `);
+      $(`#${id} button`).click(function() { set_mood(id) });
+    });
     activate_mood(mood);
   });
 }
 
-// setup click event handlers for buttons
-$("#super button").click(function() { set_mood("super") });
-$("#ok    button").click(function() { set_mood("ok")    });
-$("#down  button").click(function() { set_mood("down")  });
+function show_mood(id) {
+  var mood = moods[id];
+  $("#mood").html(`
+    <div class="card mb-4 rounded-3 shadow-sm border-${mood.color} text-center">
+      <div class="card-header py-3 text-white bg-${mood.color} border-${mood.color}">
+        <h4 class="my-0 fw-normal">${mood.header}</h4>
+      </div>
+      <div class="card-body">
+        ${mood.body}
+      </div>
+    </div>
+  `);
+}
 
-// get current mood to intialize page
-$.get("/api/mood", function(mood) {
-  activate_mood(mood);
-});
+function activate_mood(mood) {
+  var panel = $("#" + mood),
+      color = moods[mood].color;
+  panel.find(".card-header").addClass("bg-" + color);
+
+  var button = panel.find("button");
+  button.prop("disabled", true);
+  button.removeClass("btn-outline-" + color);
+  button.addClass("btn-" + color);
+}
+
+function deactivate_mood(mood) {
+  var panel = $("#" + mood),
+      color = moods[mood].color;
+  panel.find(".card-header").removeClass("bg-" + color);
+
+  var button = panel.find("button");
+  button.prop("disabled", false);
+  button.addClass("btn-outline-" + color);
+  button.removeClass("btn-" + color);
+}
+
+function set_mood(mood) {
+  post("/api/mood", { "mood" : mood }, function() {
+    $.each(moods, function(id, mood) {
+      deactivate_mood(id);
+    });
+    activate_mood(mood);
+  });
+}
