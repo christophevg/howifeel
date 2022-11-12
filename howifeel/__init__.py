e9e9f87ab281d835293f4d50dc8a28e1bf0617b7
Christophe VG <contact@christophe.vg>
2022-11-12 19:48:02 +0100

    added dotenv for loading .env secrets

--- howifeel/__init__.py
+++ howifeel/__init__.py
@@ -6,6 +6,10 @@ __version__ = "0.0.2"
 import eventlet
 eventlet.monkey_patch()
 
+# load the environment variables for this setup from .env file
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv(usecwd=True))
+
 import howifeel.setup
 
 import pathlib
