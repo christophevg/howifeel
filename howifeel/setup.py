85c88a398f5c69629acf766a2b765983c0233600
Christophe VG <contact@christophe.vg>
2024-07-05 13:31:14 +0200

    improved logging config

--- howifeel/setup.py
+++ howifeel/setup.py
@@ -1,20 +1,20 @@
-import os
 import logging
+import os
 
-LOG_LEVEL = os.environ.get("LOG_LEVEL") or "DEBUG"
+from dotenv import load_dotenv, find_dotenv
 
 logger = logging.getLogger(__name__)
 
+# load the environment variables for this setup
+load_dotenv(find_dotenv())
+load_dotenv(find_dotenv(".env.local"))
+
+# setup logging infrastructure
+
+LOG_LEVEL = os.environ.get("LOG_LEVEL") or "INFO"
 FORMAT  = "[%(asctime)s] [%(name)s] [%(process)d] [%(levelname)s] %(message)s"
 DATEFMT = "%Y-%m-%d %H:%M:%S %z"
 
-logging.basicConfig(level=logging.DEBUG, format=FORMAT, datefmt=DATEFMT)
+logging.basicConfig(level=LOG_LEVEL, format=FORMAT, datefmt=DATEFMT)
 formatter = logging.Formatter(FORMAT, DATEFMT)
-
-# adjust gunicorn logger to global level and formatting 
-gunicorn_logger = logging.getLogger("gunicorn.error")
-gunicorn_logger.handlers[0].setFormatter(formatter)
-gunicorn_logger.setLevel(logging.getLevelName(LOG_LEVEL))
-
-logging.getLogger("gunicorn.error").setLevel(logging.INFO)
 logging.getLogger().handlers[0].setFormatter(formatter)
